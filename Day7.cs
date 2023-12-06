1ae133e8ccc48762847ef2eb337db03536192339
domiendemol <domiendemol@gmail.com>
2023-12-06 11:22:08 +0100

    prepare for day  7

---
+++ Day7.cs
@@ -0,0 +1,11 @@
+namespace AoC2023;
+
+public class Day7
+{
+    public void Run()
+    {
+        List<string> lines = File.ReadAllText("input/day7.txt").Trim().Split('\n').Where(s => s.Length > 0).ToList();
+
+        
+    }
+}
